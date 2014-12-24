8f8dcb8621ef60e6f4bf3bd29f8a970f7c5e5bea
Barry Peddycord III <ishara@isharacomix.org>
2014-12-23 23:28:57 -0500

    Working on the demo page

--- js/gameboy/other/terminal.js
+++ js/gameboy/other/terminal.js
@@ -1,5 +1,5 @@
 function cout(message, colorIndex) {
-	var terminal_output = document.getElementById("terminal_output");
+	/*var terminal_output = document.getElementById("terminal_output");
 	if ((colorIndex != 0 || DEBUG_MESSAGES) && (colorIndex != -1 || DEBUG_WINDOWING)) {
 		var lineout = document.createElement('span');
 		lineout.appendChild(document.createTextNode(message));
@@ -17,11 +17,11 @@ function cout(message, colorIndex) {
 		terminal_output.appendChild(lineout);
 		terminal_output.appendChild(document.createElement('br'));
 		terminal_output.scrollTop = terminal_output.scrollHeight - terminal_output.clientHeight;
-	}
+	}*/
 }
 function clear_terminal() {
-	var terminal_output = document.getElementById("terminal_output");
+	/*var terminal_output = document.getElementById("terminal_output");
 	while (terminal_output.firstChild != null) {
 		terminal_output.removeChild(terminal_output.firstChild);
-	}
+	}*/
 }
